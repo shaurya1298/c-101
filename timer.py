a43c672a364e1aa2a67afa5124f2bcfb677d93b1
shaurya <85220653+shaurya1298@users.noreply.github.com>
2023-09-16 12:56:56 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,14 @@
+import time
+import random
+
+def countdown():
+    secounds = int(input("enter the value of seconds : "))
+    while secounds > 0:
+         secounds=secounds-1
+         print(secounds)
+countdown()         
+
+
+   
+    
+
